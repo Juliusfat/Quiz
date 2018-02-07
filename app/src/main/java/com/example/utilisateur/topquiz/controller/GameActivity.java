932cf272cf97760b29f23ecb5e382c0f2d99292c
Cyrille P <Cyrille.paillard@free.fr>
2018-02-07 20:59:30 +0100

    enregistrement du score et du user sur le telephone par SharedPreferences

--- app/src/main/java/com/example/utilisateur/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/example/utilisateur/topquiz/controller/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.utilisateur.topquiz.controller;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -29,6 +30,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private QuestionBank mQuestionBank;
     private Question mCurrentQuestion;
     private int mScore;
+    public static final String BUNDLE_EXTRA_SCORE = "BUNDLE_EXTRA_SCORE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,7 +103,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        finish();
+                        Intent intent = new Intent();
+                        intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                        setResult(RESULT_OK, intent);
+                        finish();;
                     }
                 })
                 .create()
--- app/src/main/java/com/example/utilisateur/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/example/utilisateur/topquiz/controller/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.utilisateur.topquiz.controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -22,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText mNameInput;
     private Button mPlayButton;
     private user mUser;
+    private static final int GAME_ACTIVITY_REQUEST_CODE = 42;
+    private SharedPreferences preferences;
+    private String firstname;
+    private int oldscore;
 
 
     @Override
@@ -30,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mUser = new user();
+        preferences  = getPreferences(MODE_PRIVATE);;
+        firstname = getPreferences(MODE_PRIVATE).getString("firstname", null);
 
         //recuperation des variables xml
 
@@ -37,40 +44,77 @@ public class MainActivity extends AppCompatActivity {
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
 
-        // bloquer le button
-        mPlayButton.setEnabled(false);
+
+        helloUser();
+
 
         mNameInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
+                }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // This is where we'll check the user input
-                mPlayButton.setEnabled(s.toString().length() != 0);
-            }
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    // This is where we'll check the user input
+                    mPlayButton.setEnabled(s.toString().length() != 0);
+                }
 
-            @Override
-            public void afterTextChanged(Editable s) {
+                @Override
+                public void afterTextChanged(Editable s) {
 
-            }
+                }
 
-        });
+            });
+
+
+
 
         mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstname = mNameInput.getText().toString();
+                firstname = mNameInput.getText().toString();
                 mUser.setFirstName(firstname);
                 // The user just clicked
+                SharedPreferences preferences  = getPreferences(MODE_PRIVATE);;
+                preferences.edit().putString("firstname", mUser.getFirstName()).apply();
+                //on lance l'activité GAME
                 Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(gameActivityIntent);
+                startActivityForResult(gameActivityIntent, GAME_ACTIVITY_REQUEST_CODE);
+
+
 
             }
         });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (GAME_ACTIVITY_REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
+            // Fetch the score from the Intent
+
+            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE, 0);
+            preferences.edit().putInt("Score", score).apply();
+            helloUser();
+
+        }
     }
 
+    protected void helloUser () {
+
+        if (firstname == null) {
+            // bloquer le button
+            mPlayButton.setEnabled(false);
+            mGreetingText.setText("Hello ! What's your name?");
+
+        } else {
+            oldscore = getPreferences(MODE_PRIVATE).getInt("Score", 0);
+            mGreetingText.setText("Welcome back, " + firstname + "\n Your lastest score was " + oldscore + ", will you do better this time ?");
+            mNameInput.setText(firstname);
+            mNameInput.setSelection(firstname.length());
+        }
+
+
+    }
 
 }
