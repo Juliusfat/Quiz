88ca5f9a1d86976e01008bc4e132001ebca7ef7e
Cyrille P <Cyrille.paillard@free.fr>
2018-02-13 22:20:16 +0100

    mise en forme de l'activité des scores

--- app/src/main/java/com/example/utilisateur/topquiz/controller/BestActivity.java
+++ app/src/main/java/com/example/utilisateur/topquiz/controller/BestActivity.java
@@ -3,6 +3,8 @@ package com.example.utilisateur.topquiz.controller;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.utilisateur.topquiz.R;
@@ -10,12 +12,16 @@ import com.example.utilisateur.topquiz.model.DatabaseHandler;
 import com.example.utilisateur.topquiz.model.user;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class BestActivity extends AppCompatActivity {
 
     private DatabaseHandler db;
     private ListView userListView;
+    private Button mScoreButton;
+    private Button mNameButton;
     private List<user> userList = new ArrayList<>();
 
     @Override
@@ -25,7 +31,7 @@ public class BestActivity extends AppCompatActivity {
 
         this.db = new DatabaseHandler(this);
 
-        String sql = "SELECT id, first_name, score FROM user";
+        String sql = "SELECT id, first_name, score FROM user ORDER BY score DESC LIMIT 5";
         Cursor cursor = this.db.getReadableDatabase().rawQuery(sql,null);
 
         // boucle sur le curseur
@@ -39,10 +45,28 @@ public class BestActivity extends AppCompatActivity {
 
         cursor.close();
 
-        userListView = findViewById(R.id.scoreListView);
+        mScoreButton = findViewById(R.id.activity_list_score_btn);
+        mNameButton = findViewById(R.id.activity_list_alpha_btn);
 
-        UserArrayAdapter userAdapter = new UserArrayAdapter(this, userList);
-        userListView.setAdapter(userAdapter);
+        mScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userListView = findViewById(R.id.scoreListView);
+
+                UserArrayAdapter userAdapter = new UserArrayAdapter(BestActivity.this, userList);
+                userListView.setAdapter(userAdapter);
+
+            }
+        });
+
+        /*Collections.sort(userList, new Comparator<user>() {
+            @Override
+            public int compare(user user1, user user2) {
+                return user1.getMscore().compareTo(user2.getMscore());
+            }
+        });*/
+
+
 
     }
 
